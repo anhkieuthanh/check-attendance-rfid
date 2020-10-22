dbf8bc2577eaeccb03d3aae7db726f6a01612d14
anhkieuthanh <anh.kieuthanh99@gmail.com>
2020-10-22 08:42:28 +0700

    Change config for Lab used

--- lib/Config/config.h
+++ lib/Config/config.h
@@ -1,15 +1,12 @@
-#define ssid "hello"
-#define password "123456789"
-#define mqtt_server "soldier.cloudmqtt.com"
-// #define mqtt_server "mandevices.com"
+#define ssid "MANDEVICES Laboratory"
+#define password "ptndoluongc2"
+//#define mqtt_server "soldier.cloudmqtt.com"
+ #define mqtt_server "mandevices.com"
 #define mqtt_port  16306
 // #define mqtt_port  1883
-#define mqtt_topic_pub "server: card"
-#define mqtt_topic_sub "server: response"
-#define mqtt_topic_reg "server: card-register"
+#define mqtt_topic_pub "card"
+#define mqtt_topic_sub "response"
+#define mqtt_topic_reg "card-register"
 #define mqtt_user "ryuatszf"
 #define mqtt_pwd "Y8vXAN_3iQVO"
 #define FLASH_SIZE 128
-
-#define RED_PIN 13
-#define GREEN_PIN 32
