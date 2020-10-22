bfbe608c7eb1384534a1826b2447ecd94c5a2c78
anhkieuthanh <anh.kieuthanh99@gmail.com>
2020-10-22 10:52:29 +0700

    stable version

--- lib/Config/config.h
+++ lib/Config/config.h
@@ -1,11 +1,13 @@
-#define ssid "MANDEVICES Laboratory"
-#define password "ptndoluongc2"
+// #define ssid "MANDEVICES Laboratory"
+// #define password "ptndoluongc2"
+#define ssid "hello"
+#define password "123456789"
 //#define mqtt_server "soldier.cloudmqtt.com"
- #define mqtt_server "mandevices.com"
-#define mqtt_port  16306
-// #define mqtt_port  1883
+#define mqtt_server "mandevices.com"
+//#define mqtt_port  16306
+ #define mqtt_port  1883
 #define mqtt_topic_pub "card"
-#define mqtt_topic_sub "response"
+#define mqtt_topic_sub "server:response"
 #define mqtt_topic_reg "card-register"
 #define mqtt_user "ryuatszf"
 #define mqtt_pwd "Y8vXAN_3iQVO"
